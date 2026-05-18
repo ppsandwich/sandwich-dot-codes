a892622c0cf296d138c7f57a42d71d7878034ef1
ppsandwich <dfgibbs@gmail.com>
2026-05-18 10:35:06 +1000

    Enhance animations for DoodleAccent components in HeroSection and FeaturedProjectsSection

--- src/components/projects/FeaturedProjectsSection.tsx
+++ src/components/projects/FeaturedProjectsSection.tsx
@@ -40,11 +40,21 @@ export function FeaturedProjectsSection({ projects }: FeaturedProjectsSectionPro
         </div>
       </Container>
 
-      <div className="absolute -top-12 -left-20 rotate-[-8deg] opacity-[0.05] pointer-events-none hidden lg:block">
-        <DoodleAccent variant="dot-cluster" color="#D98B73" size={400} />
+      <div
+        className="absolute -top-12 -left-20 animate-float opacity-[0.05] pointer-events-none hidden lg:block"
+        style={{ animationDuration: "13s", animationDelay: "-3s" }}
+      >
+        <div className="rotate-[-8deg]">
+          <DoodleAccent variant="dot-cluster" color="#D98B73" size={400} />
+        </div>
       </div>
-      <div className="absolute -bottom-16 -right-24 rotate-[10deg] opacity-[0.06] pointer-events-none hidden lg:block">
-        <DoodleAccent variant="star" color="#9FB06F" size={420} />
+      <div
+        className="absolute -bottom-16 -right-24 animate-float opacity-[0.06] pointer-events-none hidden lg:block"
+        style={{ animationDuration: "15s", animationDelay: "-8s" }}
+      >
+        <div className="rotate-[10deg]">
+          <DoodleAccent variant="star" color="#9FB06F" size={420} />
+        </div>
       </div>
     </Section>
   );
